f59cad8aec524d58e33bde98678b17b00e5386b1
Yugensoft <contact@yugensoft.com>
2018-08-23 01:09:54 +0700

    Creator improvement

--- resources/assets/js/components/Create.js
+++ resources/assets/js/components/Create.js
@@ -12,15 +12,25 @@ export default class Create extends Component {
         this.setState({'create_name': event.target.value});
     }
 
-    render() {
+    getUrl() {
         const name = encodeURI(this.state.create_name);
+        return `${this.props.rootUrl}/person/create?name=${name}`
+    }
+
+    handleKeyPress(event) {
+        if (event.key === 'Enter') {
+            window.location = this.getUrl();
+        }
+    }
+
+    render() {
         return (
             <div className="create dark-inputs">
                 <label htmlFor="create_name">Create New</label><br />
                 <input type="text" name="create_name" id="create_name" value={this.state.create_name}
                        onChange={this.createNameChange.bind(this)}
                 />
-                <a href={`${this.props.rootUrl}/person/create?name=${name}`}><button className="btn btn-sm">Create</button></a>
+                <a href={this.getUrl()}><button className="btn btn-sm">Create</button></a>
             </div>
         );
     }
